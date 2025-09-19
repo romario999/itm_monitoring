836ca6ce2351e5b786d8117b5e8b54af98f6adaf
Roman Bondarenko <rbondarenko211@gmail.com>
2025-09-19 22:17:12 +0300

    added bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = ""
+    bucket       = "myawsitmbucket"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
