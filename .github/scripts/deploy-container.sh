4ce13a67785b8be9ca9e8318ab2fbc51db482f01
Roman Bondarenko <rbondarenko211@gmail.com>
2025-09-25 14:33:23 +0300

    changed workflows for monitoring 81

--- .github/scripts/deploy-container.sh
+++ .github/scripts/deploy-container.sh
@@ -99,7 +99,7 @@ if [[ "$MICROSERVICE_NAME" == "prometheus" ]]; then
     --document-name "AWS-RunShellScript" \
     --parameters 'commands=[
       "cat <<EOT > /home/ec2-user/cloudwatch-config.yml
-region: eu-central-1
+region: us-east-1
 metrics:
   - aws_namespace: AWS/EC2
     aws_metric_name: CPUUtilization
@@ -122,7 +122,7 @@ metrics:
       Currency: [USD]
     aws_statistics: [Maximum]
     period_seconds: 3600
-    range_seconds: 3600
+    range_seconds: 86400
     delay_seconds: 600
     aws_region: us-east-1
 EOT",
